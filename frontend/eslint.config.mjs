07dbfd36443bbce70197c0e15e99da9c6839adb2
tejasshri <tejasshrishekhar08@gmail.com>
2025-07-11 11:37:43 +0530

    update

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -13,10 +13,21 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    rules: {
-      "@next/next/no-img-element": "off",
+     rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
   },
 ];
+
+// const eslintConfig = [
+//   ...compat.config({
+//     extends: ['next'],
+//     rules: {
+//       'react/no-unescaped-entities': 'off',
+//       '@next/next/no-page-custom-font': 'off',
+//     },
+//   }),
+// ]
 
 export default eslintConfig;
