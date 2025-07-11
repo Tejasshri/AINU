6d932fcfe47446607c0b42c1567c6e1507361d61
tejasshri <tejasshrishekhar08@gmail.com>
2025-07-11 11:55:34 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,7 @@ import patientRouter from "./routes/patient.routes.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["https://ainu.onrender.com"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", patientRouter);
